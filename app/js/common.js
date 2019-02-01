245f5810a94351f1ae75b4f4234a73e9d7dfe382
YakovSPb <prospektpro673@gmail.com>
2019-02-01 21:45:34 +0400

    New commit

---
+++ app/js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+
+
+    $(document).ready(function() {
+
+        //E-mail Ajax Send
+        $("form").submit(function() { //Change
+            var th = $(this);
+            $.ajax({
+                type: "POST",
+                url: "mail.php", //Change
+                data: th.serialize()
+            }).done(function() {
+                alert("Thank you!");
+                setTimeout(function() {
+                    // Done Functions
+                    th.trigger("reset");
+                }, 1000);
+            });
+            return false;
+        });
+
+    });
+
+});
+
+
+$(window).on('load', function(){
+    $('.preloader').delay(1000).fadeOut('slow');
+})
